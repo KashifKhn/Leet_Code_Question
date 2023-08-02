74e4872b5ef6a303e6f8ca656cbd997bb5037277
Kashif Khan <KashifKhnx04@gmail.com>
2023-08-02 19:13:51 +0500

    add question

---
+++ MaximumSubarray_53.java
@@ -0,0 +1,18 @@
+public class MaximumSubarray_53 {
+    public static void maxSubArray(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > maxSum) maxSum = sum;
+            if (sum < 0) sum = 0;
+        }
+        System.out.println("Sum is " + maxSum);
+    }
+    public static void main(String[] args) {
+        //        int[] arr = {2, 4, 6, 8, 10};
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//        int[] arr = {1};
+        maxSubArray(arr);
+    }
+}
