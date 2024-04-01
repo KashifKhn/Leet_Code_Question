1be0cf90fd12f8464a4227facb09fbecb36124b7
KashifKhn <kashifkhnx04@gmail.com>
2024-04-01 10:15:34 +0500

    adding more question

---
+++ SplitTheArray_3046.java
@@ -0,0 +1,26 @@
+public class SplitTheArray_3046 {
+    public static boolean isPossibleToSplit(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 2, 2, 3, 4 };
+        // int[] nums = { 1, 1, 1, 2, 3, 4 };
+        if (isPossibleToSplit(nums)) {
+            System.out.println("spit");
+        } else {
+            System.out.println("not");
+        }
+    }
+}
